58d70c9d8e2de79117a608ad45597c2d475dc854
ocephraim <ocephraim@gmail.com>
2019-09-27 15:16:34 +0100

    javasccript

---
+++ js/data.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+        id: 1,
+        image: "linear-gradient(0deg, #828282, #828282), url(../assets/property.jpg) no-repeat",
+        type: "FOR RENT",
+        propertyName: "4 Bedroom Apartment",
+        address: "Yaba, Lagos",
+        price: "450k/year",
+        bed: 4,
+        bath: 2
+    },
+    {
+        id: 2,
+        image: "linear-gradient(0deg, #828282, #828282), url(../assets/property.jpg) no-repeat",
+        type: "FOR RENT",
+        propertyName: "4 Bedroom Apartment",
+        address: "Yaba, Lagos",
+        price: "450k/year",
+        bed: 4,
+        bath: 6
+    },
+    {
+        id: 3,
+        image: "linear-gradient(0deg, #828282, #828282), url(../assets/property.jpg) no-repeat",
+        type: "FOR RENT",
+        propertyName: "4 Bedroom Apartment",
+        address: "Yaba, Lagos",
+        price: "450k/year",
+        bed: 4,
+        bath: 6
+    },
+    {
+        id: 4,
+        image: "linear-gradient(0deg, #828282, #828282), url(../assets/property.jpg) no-repeat",
+        type: "FOR RENT",
+        propertyName: "4 Bedroom Apartment",
+        address: "Yaba, Lagos",
+        price: "450k/year",
+        bed: 4,
+        bath: 6
+    },
+    {
+        id: 5,
+        image: "linear-gradient(0deg, #828282, #828282), url(../assets/property.jpg) no-repeat",
+        type: "FOR RENT",
+        propertyName: "4 Bedroom Apartment",
+        address: "Yaba, Lagos",
+        price: "450k/year",
+        bed: 4,
+        bath: 0
+    },
+    {
+        id: 5,
+        image: "linear-gradient(0deg, #828282, #828282), url(../assets/property.jpg) no-repeat",
+        type: "FOR RENT",
+        propertyName: "4 Bedroom Apartment",
+        address: "Yaba, Lagos",
+        price: "450k/year",
+        bed: 4,
+        bath: 40
+    }
+];
---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+const ppty = document.querySelectorAll(".property");
+
+const createListings = function(listing) {
+    let image = document.querySelectorAll(".img");
+    let type = document.querySelectorAll(".type");
+    let propertyName = document.querySelectorAll(".propertyName");
+    let address = document.querySelectorAll(".address");
+    let price = document.querySelectorAll(".price");
+    let bed = document.querySelectorAll(".bed");
+    let bath = document.querySelectorAll(".bath");
+
+    for (var i = 0; i < listing.length; i++) {
+        image[i].style.background = listing[i].image;
+        image[i].style.backgroundBlendMode = "overlay, normal";
+        image[i].style.backgroundSize = "cover";
+        type[i].innerHTML = listing[i].type;
+        propertyName[i].innerHTML = listing[i].propertyName;
+        address[i].innerHTML = `&nbsp; &#8226; &nbsp ${listing[i].address}`;
+        price[i].innerHTML = `&#8358; ${listing[i].price}`;
+        bed[i].innerHTML = `${listing[i].bed} <i class="fas fa-bed"></i>`;
+        bath[i].innerHTML = `${listing[i].bath} <i class="fas fa-bath"></i>`;
+    }
+}
+
+ppty.forEach (function(e) {
+    this.append(createListings(data));
+});
